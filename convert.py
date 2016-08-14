45ec097ab319ad3ae4ee88799f034be0b38d2111
Lucas Rodriguez <rodriguez.lucas.e@gmail.com>
2016-08-14 19:40:00 -0300

    script para convertir un numero a la base que necesitamos

---
+++ convert.py
@@ -0,0 +1,37 @@
+import sys
+
+def toBase3(n):
+	if n < 3:
+		res = []
+		res.append(n)
+		return res
+	else:
+		res = toBase3(int(n/3))
+		res.append(n%3)
+		return res
+
+
+ls = toBase3(int(sys.argv[1]))
+ls.reverse()
+res = [0] * (len(ls) + 1)
+i = 0
+for d in ls:
+	if d == 2:
+		# si tiene un 2 lo puedo reemplazar sumando la potencia que sigue y restando la potencia acutal
+		# ej: (6)_10 == (20)_3 <==> 2*3^1 + 0*3^0 <==> 1*3^2 - 1*3^1 + 0*3^0  
+		res[i] = res[i] - 1
+		res[i+1] = res[i+1] + 1
+	elif d == 1:
+		if res[i] == 1:
+			# entonces tendria un 2, tengo que dejar -1
+			res[i] = -1
+			res[i+1] = res[i+1] + 1
+		else:
+			res[i] = 1
+	i = i + 1
+
+res.reverse()
+print res
+
+		
+
