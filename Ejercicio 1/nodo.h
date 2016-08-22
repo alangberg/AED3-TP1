d242cea14cfc91f94d4879019d02b8c406b7683f
nicoluce <nicolas.p.luce@gmail.com>
2016-08-22 15:33:04 -0300

    Agrego los comentarios del Ej1

--- Ejercicio 1/nodo.h
+++ Ejercicio 1/nodo.h
@@ -13,17 +13,17 @@ struct Nodo {
 
 	bool valido;
 
-	vector<Nodo*> aa;
-	vector<Nodo*> cc;
-	vector<Nodo*> ac;
+	vector<Nodo*> aa; // vector de ramas del movimiento AA
+	vector<Nodo*> cc; // vector de ramas del movimiento CC
+	vector<Nodo*> ac; // vector de ramas del movimiento AC
 
-	vector<Nodo*> a;
-	vector<Nodo*> c;
+	vector<Nodo*> a; // vector de ramas del movimiento A
+	vector<Nodo*> c; // vector de ramas del movimiento C
 
-	int vel;
+	int vel; // la velocidad la guardo en cada nodo
 };
 
-
+// Contructor de Nodo
 Nodo::Nodo() {
 	vector<Nodo*> vacio;
 	this->aa = vacio;
@@ -34,11 +34,13 @@ Nodo::Nodo() {
 	this->c = vacio;
 
 	this->vel = 0;
-	this->valido = true;
+	this->valido = true; // por default todos los nodos son soluciones validas
 }
 
+// Destructor de Nodo, es recursivo
+// borro cada nodo de cada vector
 Nodo::~Nodo() {
-	for (int i = 0; i < this->aa.size(); ++i) delete this->aa[i];	
+	for (int i = 0; i < this->aa.size(); ++i) delete this->aa[i];
 	for (int i = 0; i < this->cc.size(); ++i) delete this->cc[i];
 	for (int i = 0; i < this->ac.size(); ++i) delete this->ac[i];
 
