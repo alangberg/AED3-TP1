3abdfa104f797ad55c260111f105cf6bafc68788
Nicolas Walter <nicowalter25@gmail.com>
2016-08-26 17:24:24 -0300

    subo script que anda raro para experimenar

---
+++ Experimentos2/correr_experimentos.py
@@ -0,0 +1,64 @@
+#encoding: utf-8
+import math
+
+"""modo de uso: python correr_experimentos.py 1(repetir experimentos)/0(no repetir experimentos)"""
+
+ejecutable = "./tiempo"
+datoslog = "tiemposPuroPython.dat"
+datosPoda = 'tiempoPodaPython.dat'
+outputGrafico = 'ej2.png'
+rango = range(0, 10000, 500)
+repes = 1000
+labellog = u"Sin podas"
+labelPoda = u"Con podas"
+
+def funcion(a, rango):
+		return []+ map(str,map(a, rango))
+
+def armarArgumentos(ejecutable, repes, rango):
+	# Esto arma los argumentos para call:
+	# 	El primer elemento de la lista es el programa que se va a correr
+	# 	El resto son los argumentos para este programa.
+	#	O sea, si tenemos la lista l = [exec, a1, a2, a3] y hacemos call(l)
+	#   es como si escribiéramos en la terminal exec a1 a2 a3 y apretáramos enter.
+	#	Obs: todos tienen que ser strings.
+	return [ejecutable, str(repes)] + map(str, rango)
+
+if __name__ == '__main__':
+	from sys import argv
+	repetirExperimentos = bool(int(argv[1]))
+	if repetirExperimentos :
+		from subprocess import call
+
+		#'w' indica que el archivo se abre en modo escritura
+		#	Esto significa que si ya existe un archivo con el mismo nombre,
+		#  	éste SE SOBREESCRIBE CON UN ARCHIVO VACÍO.
+		#	Si no existe, se crea.
+		with open(datosPoda, 'w') as f:
+			args =  armarArgumentos(ejecutable, repes, rango)
+			print args
+			#los parámetros de call son: 
+			#	args: una lista con los argumentos del programa, como si fuera la terminal (ver armarArgumentos)
+			#	tiene parámetros opcionales stdout, stdin, stderr para redirigirlos a archivos. 
+			#	Ojo con hacer pipes, ver documentación. 
+		 	call(args, stdout=f)
+
+
+		# with open(datoslog, 'w') as f:
+		#  	caca= []
+		#  	for x in rango:
+		#  		caca+ [log(x,3)]
+		#  	map(str,caca)
+
+		#  	print caca
+		#  	call(caca, stdout=f)
+		
+		
+		#Acá se cierra f, así si lo interrumpimos mientras mide para 8,
+		#los datos anteriores están escritos en el archivo
+		
+		#'a' indica que el archivo se abre en modo append (agregar)
+		#	Esto significa que si no existe un archivo con ese nombre, se crea.
+		#	Si ya existe, se conserva y se le agrega  al final lo que se vaya a escribir.		
+	from plots import main
+	main(dataFiles=[datosPoda], output = outputGrafico, labels = [labelPoda], show = True)
---
+++ Experimentos2/ej2.cpp
@@ -0,0 +1,82 @@
+#include "ej2.h"
+
+vector<int> toBase3(int n){
+	vector<int> res;
+	if(n < 3){
+		res.push_back(n);
+		return res;
+	} else {
+		res = toBase3(n / 3);
+		res.push_back(n % 3);
+		return res;
+	}
+}
+
+vector<int> reverse(vector<int> v){
+	int j = 0;
+	vector<int> res;
+	for(int i = v.size()-1; i >= 0; --i){
+		res.push_back(v[i]);
+	}
+	return res;
+}
+
+vector<int> balancear(vector<int> v){
+	vector<int> res(v.size() + 1, 0);
+	v =reverse(v);
+
+	int i = 0;
+	for(int j = 0; j < v.size(); ++j){
+		if(v[j] == 2){
+			res[i] = res[i] - 1;
+			res[i+1] = res[i+1] + 1;
+		} else if(v[j] == 1){
+			if(res[i] == 1){
+				res[i] = -1;
+				res[i+1] = res[i+1] + 1;
+			} else {
+				res[i] = 1;
+			}
+		}
+		i++;
+	} 
+	return res;
+}
+
+void imprimirVector(vector<int> v){
+	if(v.size() == 0) return; // si no tiene nada, que no imprima nada
+	for(int i = 0; i < v.size(); ++i){
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
+
+void ej2(int n){
+	vector<int> v = balancear(toBase3(n));
+	vector<int> izquierda;
+	vector<int> derecha;
+
+	for(int i = 0; i < v.size(); ++i){
+		if(v[i] == 1){
+			izquierda.push_back(pow(3, i));
+		}else if(v[i] == -1){
+			derecha.push_back(pow(3, i));
+		}	
+	}
+
+	cout << izquierda.size() << " " << derecha.size() << endl;
+
+	sort(izquierda.begin(), izquierda.end());
+	sort(derecha.begin(), derecha.end());
+
+	imprimirVector(izquierda);
+	imprimirVector(derecha);
+}
+
+/*int main(int argc, char *argv[]){
+	int t = atoi(argv[1]);
+	
+	balancear(toBase3(t));
+
+	return 0;
+}*/
